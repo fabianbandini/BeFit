6c2c1eee6a09872e4ca0a22d846755031e9f89e2
Fabian Bandini <fabian.bandini.bd@gmail.com>
2022-12-15 12:49:09 +0100

    Add project files.

---
+++ BeFIt/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace BeFIt;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ BeFIt/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace BeFIt;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
